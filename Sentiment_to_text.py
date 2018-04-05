6b7fcae1b399953bc850ca0628e1c467d7145a2c
JASON JOHNSON <JASJOHN3@uat.edu>
2018-04-05 16:44:21 -0700

    Testing Initial Commit and update

--- Sentiment_to_text.py
+++ Sentiment_to_text.py
@@ -13,10 +13,10 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-file = open('Readme.txt','r')
-message = file.read()
+Readme_file = open('Readme.txt','r')
+message = Readme_file.read()
 print(message)
-file.close()
+Readme_file.close()
 print("Please Enter the Word you would like to perform a sentiment analysis on!")
 User_Input = input()
 User_Tweet_Search_Size = int(input("Please enter how large you want your search to be: "))
@@ -27,16 +27,18 @@ public_tweets = api.search(q=User_Input, count= User_Tweet_Search_Size)
 counter = 0
 file = open(User_Input+'.txt','w')
 message = file.write(User_Input)
-print(message)
-file.close()
-for tweet in public_tweets:
 
-     counter = counter + 1
-     print("============================================================================================================================================================================================")
-     print("|",counter,"|")
-     print(tweet.text)
-     print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-     # Step 4 Perform Sentiment Analysis on Tweets
-     analysis = TextBlob(tweet.text)
-     print(analysis.sentiment)
-     print("============================================================================================================================================================================================")
+
+for tweet in public_tweets:
+    counter = counter + 1
+    string_counter = str(counter)
+    counter_write=file.write(string_counter)
+    tweet_message =file.write(tweet.text)
+    analysis = TextBlob(tweet.text)
+    string_analysis = str(analysis.sentiment)
+    Sentiment_message = file.write(string_analysis)
+
+
+full_sentiment_message = file.read()
+print(full_sentiment_message)
+file.close()
